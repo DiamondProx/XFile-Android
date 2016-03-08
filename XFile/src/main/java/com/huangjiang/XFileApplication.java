6d4807191552cb435a5664382a7abe4f826db751
diamondprox <26666302@qq.com>
2016-03-08 18:29:33 +0800

    项目基本内容流程跳转

--- XFile/src/main/java/com/huangjiang/XFileApplication.java
+++ XFile/src/main/java/com/huangjiang/XFileApplication.java
@@ -2,15 +2,17 @@ package com.huangjiang;
 
 import android.app.Application;
 
-import com.huangjiang.message.XFileServer;
-
 public class XFileApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        XFileServer server=new XFileServer();
-        server.startServer();
+//        try {
+//            LocalTimeClient.startClient();
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
     }
 
 }
